5a2f3ed2e16f1611642c463d607f6577d55fdb84
mogeiwang <mogeiwang@gmail.com>
2016-07-13 11:54:36 +0800

    Use ask mode in akka

--- drama.scala
+++ drama.scala
@@ -5,8 +5,8 @@
 
 // import scala.actors._  // Actor
 // import scala.actors.Actor._  // actions
-import scala.collection.immutable.{Map => _, Set => _, _}  // Import Vector, List, Range; excludes Map, Set
-import scala.collection.mutable._  // ArrayBuffer, StringBuilder, HashMap or HashSet
+import scala.collection.immutable.{Map => _, Set => _, _}  // Vector, List, Range
+import scala.collection.mutable._  // ArrayBuffer, StringBuilder, HashMap, HashSet
 import scala.collection.JavaConversions._  // Java ops
 import scala.concurrent.duration._ // actor msg duration
 import scala.io.Source._  // file ops
@@ -28,7 +28,6 @@ import akka.pattern._
 import akka.util._
 
 
-
 object drama extends App {
   case class Msg1(x: Int)
   case class Msg2(x: Int)
@@ -36,11 +35,15 @@ object drama extends App {
   case class Msg4()
   case class Msg0(x: Int)
   case class Init()
+  case class Dest()
+  case class Done()
+  case class Fail()
   case class SetNext(agtArr: Array[Int])
 
-  val stagent = ActorSystem("stage-agent-on-akka")
+  val stagent = ActorSystem("agent-on-stage")
   val stg = stagent.actorOf(Props[Stage])
   val agt = for (i <- 0 until 3) yield stagent.actorOf(Props( new Agent(i) ))
+  implicit val timeout = Timeout(1 seconds)
 
   class Agent(id: Int) extends Actor {
     var nextAgt: Array[Int] = new Array[Int](3)
@@ -49,62 +52,84 @@ object drama extends App {
     var lower: Int = -10
     // ...
     def receive() = {
-      case Init =>
+      case Init() =>
         state = 0
         println("Agent["+id+"] is ready. State: "+state)
+        sender ! Done()
       case Msg0(i) =>
         state = 0
         println("Agent["+id+"] is reset by Agent["+i+"]")
+        sender ! Done()
       case SetNext(agtArr) =>
         nextAgt = agtArr
         for (i <- nextAgt) println("Agent["+id+"] has next Agent: "+i)
-      case Msg3 =>
+        sender ! Done()
+      case Msg3() =>
         state += 1
         if (state >= upper) {
             println("Agent["+id+"] hyperpolarized. State: "+state)
             upper += 10
             lower = -10
-            stg ! Msg1(id)
-            for (i <- nextAgt) agt(i) ! Msg0(id)
+            stg ? Msg1(id)
+            for (i <- nextAgt) agt(i) ? Msg0(id)
             }
-      case Msg4 =>
+        sender ! Done()
+      case Msg4() =>
         state -= 1
         if (state <= lower) {
             println("Agent["+id+"] depolarized. State: "+state)
             lower -= 10
             upper = 10
-            stg ! Msg2(id)
-            for (i <- nextAgt) agt(i) ! Msg0(id)
+            stg ? Msg2(id)
+            for (i <- nextAgt) agt(i) ? Msg0(id)
             }
+        sender ! Done()
+      case Dest() =>
+            println("Agent["+id+"] done.")
+        sender ! Done()
+      case _ =>
+        sender ! Fail()
     }
   }
 
 
   class Stage() extends Actor {
     def receive() = {
-      case Init =>
+      case Init() =>
         println("Stage is ready.")
+        sender ! Done()
       case Msg1(x) =>
         println("Checking Agent["+x+"] hyperpolarization.")
+        sender ! Done()
       case Msg2(x) =>
         println("Checking Agent["+x+"] depolarization.")
+        sender ! Done()
+      case Dest() =>
+        println("Stage done.")
+        sender ! Done()
+      case _ =>
+        sender ! Fail()
     }
   }
 
 
   // ---------------------
 
-  agt(0) ! SetNext(Array(1, 2))
-  agt(1) ! SetNext(Array(2))
-  agt(2) ! SetNext(Array(0))
+  agt(0) ? SetNext(Array(1, 2))
+  agt(1) ? SetNext(Array(2))
+  agt(2) ? SetNext(Array(0))
 
-  for (i <- agt) i ! Init
-  stg ! Init
+  for (i <- agt) i ? Init()
+  stg ? Init()
 
   for (i <- 1 to 1000; j <- 0 until 3) {
-    agt(j) ! (if (nextFloat >0.5) Msg3 else Msg4)
+    agt(j) ? (if (nextFloat >0.5) Msg3() else Msg4() )
   }
 
-  Thread sleep 100
+  for (i <- agt) i ? Dest()
+  stg ? Dest()
+
+  // this is no longer a must
+  Thread sleep 500
   stagent shutdown
 }
